faa43cdcbebb19b804ba79a5053208e0941deb31
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-02-15 14:35:41 -0800

    navbar attempt 10

---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,51 @@
+import { Close, DragHandle } from '@mui/icons-material';
+import { IconButton } from '@mui/material'
+import React, { useState } from 'react'
+
+export default function Navbar() {
+
+
+
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+
+
+    const toggleMenu = () => setIsMenuVisible(!isMenuVisible);
+
+
+
+    return (
+        <div className='navbar'>
+            <a className='logo' href="#">Welldone</a>
+
+            <div className='navigation'>
+
+
+                <IconButton onClick={toggleMenu} className='menuBtn'>
+
+                    {!isMenuVisible &&
+                        <DragHandle className='menuIcon'
+                        />}
+
+                    {isMenuVisible &&
+                        <Close className='menuIcon'
+                        />}
+
+                </IconButton>
+
+                <ul className={`menu ${isMenuVisible ? 'active' : 'hidden'}`}>
+                    <li className='menuItem'><a href="#home">Home</a></li>
+                    <li className='menuItem'><a href="#portfolio">Portfolio</a></li>
+                    <li className='menuItem'><a href="#services">Services</a></li>
+                    <li className='menuItem'><a href="#about">About</a></li>
+                    <li className='menuItem'><a href="#pricing">Pricing</a></li>
+                    <li className='menuItem'><a href="#contact">Contact</a></li>
+
+                </ul>
+            </div>
+
+
+
+        </div>
+    )
+}
