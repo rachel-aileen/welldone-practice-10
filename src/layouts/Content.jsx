4b2eed49854208f5758d2a40edbf03e2177c812d
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-02-15 16:59:44 -0800

    pricing intro

--- src/layouts/Content.jsx
+++ src/layouts/Content.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import PageSelect from '../PageSelect'
 
 export default function Content() {
     return (
         <div>
 
 
-            <div className='contentOverview' >
+            <div className='introContent' >
                 <h1 className='heading'>We believe in transparency</h1>
                 <h6 className='subheading'>Want a price estimate?</h6>
+                <p className='mainText' >
+                    Get it below.
+                    Please note this is only a ballpark cost and does not
+                    reflect the final cost.
+                </p>
 
+                <PageSelect />
             </div>
+
+
         </div>
     )
 }
