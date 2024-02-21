52c2a8c78966b216251c0fef0cdebccac9f02c9f
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-02-20 19:21:08 -0800

    rodarte (name edit)

--- src/PageSelector.jsx
+++ src/PageSelector.jsx
@@ -3,7 +3,8 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import { Box } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
+import { AcUnit, AssistWalker, AutoAwesome, FilterVintage } from '@mui/icons-material';
 
 export default function PageSelector() {
     const [age, setAge] = React.useState('');
@@ -20,18 +21,38 @@ export default function PageSelector() {
                 borderBottom: '2px solid #F6F2F2;'
             }}
         >
-            <FormControl variant="standard" sx=
+            <FormControl variant="standard"
+                sx=
                 {{
+
                     width: '100%',
                     '.MuiInput-underline:before, .MuiInput-underline:after': { display: 'none' }
                 }}>
                 <InputLabel
-
+                    disableAnimation
                     id="demo-simple-select-standard-label"
-                    sx={{ color: '#F2FEDC', fontSize: '7px !important', '&.Mui-focused': { color: '#F2FEDC', fontSize: '7px !important' } }}
+                    sx={{
+                        color: '#F2FEDC', // Ensure color applies at all times
+                        '&.Mui-focused': {
+                            color: '#F2FEDC', // Ensure color remains pink when focused
+                        },
+                        position: 'relative', // Adjusted to keep label fixed
+                        transform: 'none', // Prevents animation
+                    }}
                 >
-                    <span style={{ color: 'red', marginRight: 10 }}>*</span>How many pages will your website have?
+                    <Stack
+                        direction="row"
+                        justifyContent="flex-start"
+                        alignItems="center"
+                        spacing={2}
+                    >
+                        <FilterVintage sx={{ color: '#E03D5A' }} />
+                        <Typography className='inputLabel'>How many pages will your website have?</Typography>
+                    </Stack>
                 </InputLabel>
+
+
+
                 <Select
                     required
                     labelId="demo-simple-select-standard-label"
@@ -39,8 +60,9 @@ export default function PageSelector() {
                     value={age}
                     onChange={handleChange}
                     label="Age"
+
                     sx={{
-                        color: '#F6F2F2', // Text and icon color
+                        color: '#F6F2F2',
                         '& .MuiSvgIcon-root': { color: '#F6F2F2' }, // Dropdown icon color
                         // Hide the default underline
                         '& .MuiInput-underline:before, .MuiInput-underline:hover:not(.Mui-disabled):before': {
@@ -54,6 +76,7 @@ export default function PageSelector() {
                     <MenuItem value={10}>Ten</MenuItem>
                     <MenuItem value={20}>Twenty</MenuItem>
                     <MenuItem value={30}>Thirty</MenuItem>
+                    <MenuItem value={40}>Thirty</MenuItem>
                 </Select>
             </FormControl>
         </Box>
