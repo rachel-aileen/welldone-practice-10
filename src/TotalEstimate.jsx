5bcbdd1eb3c5f22674c271f365ddaefd1f6502f2
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-03-01 14:13:24 -0800

    total estimate text css edits

--- src/TotalEstimate.jsx
+++ src/TotalEstimate.jsx
@@ -2,27 +2,33 @@ import * as React from 'react';
 import { Box, Typography } from '@mui/material';
 
 export default function TotalEstimate() {
-
-
-
     const whiteColor = '#F6F2F2';
 
     return (
         <Box
             sx={{
                 m: 1,
-                p: 2, // Add some padding inside the box
-                width: '100%', // The box takes the full width
-                backgroundColor: 'transparent', // Make the inside of the box transparent
-                color: whiteColor, // Set the text color to blue to match the outline
-                border: '2px solid #E03D5A', // Blue border for the outline
-                borderRadius: '0', // Sharp corners
-                display: 'flex', // Use Flexbox for alignment
-                alignItems: 'center', // Align items vertically
-                height: '56px', // Match the height of MUI text field
+                p: 2,
+                width: '100%',
+                backgroundColor: 'transparent',
+                color: whiteColor,
+                border: '2px solid #E03D5A',
+                borderRadius: '0',
+                display: 'flex',
+                alignItems: 'center',
+                height: '56px',
             }}
         >
-            <Typography variant="body1">Total Estimate:</Typography>
+            <Typography
+                variant="body1"
+                sx={{
+                    fontSize: 'clamp(1rem, 6vw, 1.3rem)',
+                    fontFamily: 'Nunito Sans',
+                    fontWeight: 500,
+                }}
+            >
+                Total Estimate:
+            </Typography>
         </Box>
     );
 }
